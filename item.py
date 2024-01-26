1e4020452799c25496ba76a4f592cd7d13d917ea
Piotr Chylak <piot.chylak@wp.pl>
2024-01-26 07:30:47 +0000

    Dodanie atrybutu item_type. (bohater ma ograniczenie na 1 typ ekwipunku założony w jednym momencie)

--- item.py
+++ item.py
@@ -1,8 +1,9 @@
 class Item:
-    def __init__(self, name, hp_bonus, dmg_bonus, initiative_bonus):
+    def __init__(self, name, hp_bonus, dmg_bonus, initiative_bonus, item_type):
         self.character = "$"
         self.name = name
-        self.description = f"item: {name}"
+        self.description = f"{name}"
         self.hp_bonus = hp_bonus
         self.dmg_bonus = dmg_bonus
         self.initiative_bonus = initiative_bonus
+        self.item_type = item_type
